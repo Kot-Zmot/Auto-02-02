23cab078b5a0966a187da7003d04c0de87dd7735
Kot-Zmot <meledin.av52@gmail.com>
2023-04-24 22:42:14 +0300

    test works correct 03

--- src/test/java/CardDeliveryFormTest.java
+++ src/test/java/CardDeliveryFormTest.java
@@ -19,7 +19,7 @@ public class CardDeliveryFormTest {
     void webFormSuccessTest() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Нижний Новгород");
-        String currentDate = generateDate(3, "dd.MM.yyyy");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Чехов Антон");
@@ -27,7 +27,7 @@ public class CardDeliveryFormTest {
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
