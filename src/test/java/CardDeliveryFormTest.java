a2dccd49e431cd85fcfcbd5abaff4b4909ac3e3c
Kot-Zmot <meledin.av52@gmail.com>
2023-04-24 23:09:10 +0300

    test works correct 09

--- src/test/java/CardDeliveryFormTest.java
+++ src/test/java/CardDeliveryFormTest.java
@@ -27,7 +27,7 @@ public class CardDeliveryFormTest {
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(45))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
     }
 }
